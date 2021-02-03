a4904c4c638abdda00f1da4fdab18af82b53d848
anasir514 <anasir514@gmail.com>
2021-02-03 10:30:29 +0000

    nm

--- djan_blog/blog/views.py
+++ djan_blog/blog/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'auther': 'john',
+        'title': ' blog post',
+        'date_posted':'ugust 27,2019'
+    },
+    {
+        'auther': 'mark',
+        'title': ' blog st',
+        'date_posted':'ugust 27,2029'
+    }
+]
+
 def home_views(request,*args,**kwargs):
-    return render(request, "blog/home.html", context={}, status=200)
+    context = {
+        'posts' : posts
+    }
+    return render(request, "blog/home.html", context, status=200)
 
 
 def about_views(request,*args,**kwargs):
-    return render(request, "blog/about.html", context={}, status=200)
+    return render(request, "blog/about.html", {'title':'about'}, status=200)
