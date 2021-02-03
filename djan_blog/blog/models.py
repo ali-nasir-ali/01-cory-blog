42bb5bbf873e7cc32c6c817532a921727358ee81
anasir514 <anasir514@gmail.com>
2021-02-03 11:08:44 +0000

    time addded

--- djan_blog/blog/models.py
+++ djan_blog/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# each class is going to be a tables in a database
+class Post(models.Model): 
+# now the atributes is going to be a differnt filed in the database
+     title = models.CharField(max_length=100)
+     content = models.TextField()
+     date_posted = models.DateTimeField(default=timezone.now)
+     
